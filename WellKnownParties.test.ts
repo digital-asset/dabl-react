d813c31448d77b1e56fab37c4a02228b7e1dcce0
Leonid Rozenberg <leonid.rozenberg@digitalasset.com>
2020-07-22 14:37:50 -0400

    Remove file that was a security concern, enable mocking in individual tests.

--- WellKnownParties.test.ts
+++ WellKnownParties.test.ts
@@ -2,6 +2,8 @@
 // Copyright (c) 2020 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
 // SPDX-License-Identifier: Apache-2.0
 
+import { enableFetchMocks } from 'jest-fetch-mock';
+enableFetchMocks()
 import fetchMock from 'jest-fetch-mock';
 import React, { ComponentType } from 'react';
 import { renderHook } from '@testing-library/react-hooks';
