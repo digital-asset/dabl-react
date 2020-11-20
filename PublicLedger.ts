5bd174d83b3364ffd26e2bd15b8da9f41939d22e
Alex Matson <alex.matson@digitalasset.com>
2020-11-20 14:11:58 -0500

    upgrade daml sdk to 1.7.0 and mark deprecated APIs, add replacements

--- PublicLedger.ts
+++ PublicLedger.ts
@@ -1,7 +1,7 @@
 import { createElement, PropsWithChildren, useEffect, useState } from 'react';
 import { Party, Template } from '@daml/types';
-import Ledger, {Query} from '@daml/ledger';
-import { createLedgerContext, FetchResult, QueryResult } from '@daml/react';
+import Ledger, {Query, StreamCloseEvent} from '@daml/ledger';
+import { createLedgerContext, FetchByKeysResult, FetchResult, QueryResult } from '@daml/react';
 
 function publicPartyEndPoint(ledgerId:string):string {
   return `api.projectdabl.com/api/ledger/${ledgerId}/public/token`;
@@ -18,7 +18,7 @@ async function fetchPublicPartyToken(ledgerId:string) : Promise<string|null> {
   }
 }
 
-const { DamlLedger, useParty, useLedger, useQuery, useFetchByKey, useStreamQuery, useStreamFetchByKey, useReload} = createLedgerContext();
+const { DamlLedger, useParty, useLedger, useQuery, useFetchByKey, useStreamQuery, useStreamQueries, useStreamFetchByKey, useStreamFetchByKeys, useReload} = createLedgerContext();
 
 type PublicProp = {
   ledgerId : string,
@@ -61,14 +61,56 @@ export function useFetchByKeyAsPublic<T extends object, K, I extends string>(tem
   return useFetchByKey(template, keyFactory, keyDeps);
 }
 
-export function useStreamQueryAsPublic<T extends object, K, I extends string>(template: Template<T, K, I>, queryFactory: () => Query<T>, queryDeps: readonly unknown[]): QueryResult<T, K, I>
+/**
+ * React Hook to query the ledger, the returned result is updated as the ledger state changes.
+ *
+ * @deprecated prefer useStreamQueriesAsPublic
+ *
+ * @typeparam T The contract template type of the query.
+ * @typeparam K The contract key type of the query.
+ * @typeparam I The template id type.
+ *
+ * @param template The template of the contracts to match.
+ * @param queryFactory A function returning a query. If the query is omitted, all visible contracts of the given template are returned.
+ * @param queryDeps The dependencies of the query (for which a change triggers an update of the result).
+ * @param closeHandler A callback that will be called if the underlying WebSocket connection fails in an unrecoverable way.
+ *
+ * @return The matching contracts.
+ */
+export function useStreamQueryAsPublic<T extends object, K, I extends string>(template: Template<T, K, I>, queryFactory: () => Query<T>, queryDeps: readonly unknown[], closeHandler: (e: StreamCloseEvent) => void): QueryResult<T, K, I>
 export function useStreamQueryAsPublic<T extends object, K, I extends string>(template: Template<T, K, I>): QueryResult<T, K, I>
-export function useStreamQueryAsPublic<T extends object, K, I extends string>(template: Template<T, K, I>, queryFactory?: () => Query<T>, queryDeps?: readonly unknown[]): QueryResult<T, K, I> {
-  return useStreamQuery(template, queryFactory, queryDeps);
+export function useStreamQueryAsPublic<T extends object, K, I extends string>(template: Template<T, K, I>, queryFactory?: () => Query<T>, queryDeps?: readonly unknown[], closeHandler?: (e: StreamCloseEvent) => void): QueryResult<T, K, I> {
+  return useStreamQuery(template, queryFactory, queryDeps, closeHandler);
 }
 
-export function useStreamFetchByKeyAsPublic<T extends object, K, I extends string>(template: Template<T, K, I>, keyFactory: () => K, keyDeps: readonly unknown[]): FetchResult<T, K, I> {
-  return useStreamFetchByKey(template, keyFactory, keyDeps);
+export function useStreamQueriesAsPublic<T extends object, K, I extends string>(template: Template<T, K, I>, queryFactory: () => Query<T>, queryDeps: readonly unknown[], closeHandler: (e: StreamCloseEvent) => void): QueryResult<T, K, I>
+export function useStreamQueriesAsPublic<T extends object, K, I extends string>(template: Template<T, K, I>): QueryResult<T, K, I>
+export function useStreamQueriesAsPublic<T extends object, K, I extends string>(template: Template<T, K, I>, queryFactory?: () => Query<T>[], queryDeps?: readonly unknown[], closeHandler?: (e: StreamCloseEvent) => void): QueryResult<T, K, I> {
+  return useStreamQueries(template, queryFactory, queryDeps, closeHandler);
+}
+
+/**
+ * React Hook to query the ledger. Same as useStreamQueryAsPublic, but query by contract key instead.
+ *
+ * @deprecated prefer useStreamFetchByKeysAsPublic
+ *
+ * @typeparam T The contract template type of the query.
+ * @typeparam K The contract key type of the query.
+ * @typeparam I The template id type.
+ *
+ * @param template The template of the contracts to match.
+ * @param queryFactory A function returning a contract key.
+ * @param queryDeps The dependencies of the query (for which a change triggers an update of the result).
+ * @param closeHandler A callback that will be called if the underlying WebSocket connection fails in an unrecoverable way.
+ *
+ * @return The matching (unique) contract, or null.
+ */
+export function useStreamFetchByKeyAsPublic<T extends object, K, I extends string>(template: Template<T, K, I>, keyFactory: () => K, keyDeps: readonly unknown[], closeHandler: (e: StreamCloseEvent) => void): FetchResult<T, K, I> {
+  return useStreamFetchByKey(template, keyFactory, keyDeps, closeHandler);
+}
+
+export function useStreamFetchByKeysAsPublic<T extends object, K, I extends string>(template: Template<T, K, I>, keyFactory: () => K[], keyDeps: readonly unknown[], closeHandler: (e: StreamCloseEvent) => void): FetchByKeysResult<T, K, I> {
+  return useStreamFetchByKeys(template, keyFactory, keyDeps, closeHandler);
 }
 
 export const useReloadAsPublic = useReload
