1ae13011113472432570573fbb54512174dc453b
Alex Matson <alex.matson@digitalasset.com>
2021-01-26 17:44:00 -0500

    dabl parties input button

--- index.ts
+++ index.ts
@@ -2,3 +2,4 @@
 export { WellKnownParties, WellKnownPartiesProvider, useWellKnownParties } from "./WellKnownParties";
 export { expiredToken, partyName as partyNameFromJwtToken } from "./JwtTokens";
 export { PublicLedger, usePartyAsPublic, useLedgerAsPublic, useQueryAsPublic, useFetchByKeyAsPublic, useStreamQueryAsPublic, useStreamQueriesAsPublic, useStreamFetchByKeyAsPublic, useStreamFetchByKeysAsPublic, useReloadAsPublic} from "./PublicLedger";
+export { PartyDetails, convertPartiesJson, DablPartiesInput } from "./DablPartiesInput";
