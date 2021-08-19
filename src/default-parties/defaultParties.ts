871bc1a8a811ca0f67149e022d570ee0a2cbab07
Alex Matson <alex.matson@digitalasset.com>
2021-08-19 14:44:22 -0400

    add default parties

---
+++ src/default-parties/defaultParties.ts
@@ -0,0 +1,69 @@
+import { Decoder, array, boolean, number, object, string } from '@mojotech/json-type-validation';
+import { detectAppDomainType, DomainType } from '../utils';
+
+const PUBLIC_DISPLAY_NAME = 'Public';
+const USER_ADMIN_DISPLAY_NAME = 'UserAdmin';
+
+interface PartyDetails {
+  displayName: string;
+  identifier: string;
+  isLocal: boolean;
+}
+
+interface LegacyAPIResponse {
+  userAdminParty?: string;
+  publicParty: string;
+}
+
+const legacyAPIDecoder: Decoder<LegacyAPIResponse> = object({
+  userAdminParty: string(),
+  publicParty: string(),
+});
+
+interface AppAPIResponse {
+  result: PartyDetails[];
+  status: number;
+}
+
+const appAPIDecoder: Decoder<AppAPIResponse> = object({
+  result: array(
+    object({
+      displayName: string(),
+      identifier: string(),
+      isLocal: boolean(),
+    })
+  ),
+  status: number(),
+});
+
+type DefaultParties = [string | undefined, string | undefined];
+
+function getPartyIdByName(list: PartyDetails[], displayName: string): string | undefined {
+  return list.find(p => p.displayName === displayName)?.identifier;
+}
+
+export async function fetchDefaultParties(): Promise<DefaultParties> {
+  try {
+    const { hostname: hn } = window.location;
+    switch (detectAppDomainType()) {
+      case DomainType.APP_DOMAIN:
+        const app_response = await fetch(`//${hn}/.hub/v1/default-parties`);
+        const app_json = await app_response.json();
+        const app_parties = appAPIDecoder.runWithException(app_json);
+        return [
+          getPartyIdByName(app_parties.result, PUBLIC_DISPLAY_NAME),
+          getPartyIdByName(app_parties.result, USER_ADMIN_DISPLAY_NAME),
+        ];
+      case DomainType.LEGACY_DOMAIN:
+        const legacy_response = await fetch(`//${hn}/.well-known/dabl.json`);
+        const legacy_json = await legacy_response.json();
+        const legacy_parties = legacyAPIDecoder.runWithException(legacy_json);
+        return [legacy_parties.publicParty, legacy_parties.userAdminParty];
+      default:
+        throw new Error('App not running on Daml Hub');
+    }
+  } catch (error) {
+    console.error(`Error determining well known parties ${JSON.stringify(error)}`);
+    return [undefined, undefined];
+  }
+}
