fd4ea2c3ceafe0c1fa5e5291afc530d326ae795f
Alex Matson <alex.matson@digitalasset.com>
2021-09-16 11:35:44 -0400

    Use a proper bash script for publishing instead of makefile

--- .circleci/publish.sh
+++ .circleci/publish.sh
@@ -3,8 +3,6 @@
 set -euo pipefail
 
 GIT_TAG=$(git tag --contains)
-GIT_TAG="v1.0.0-rc.1"
-RELEASE_TAG="latest"
 
 if [[ "$GIT_TAG" = "" ]]
 then
@@ -14,10 +12,11 @@ fi
 
 echo "Found git tag: '$GIT_TAG'..."
 
+RELEASE_TAG="latest"
 if [[ $GIT_TAG =~ ^v.*rc.* ]]
 then
     RELEASE_TAG="prerelease"
 fi
 
 echo "Publishing with '--tag $RELEASE_TAG'..."
-npm publish --access public --tag $RELEASE_TAG --dry-run
+npm publish --access public --tag $RELEASE_TAG
