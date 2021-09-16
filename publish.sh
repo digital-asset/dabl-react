f85a23d74ba3e3ff2fb21513b6cfb0341fa347f5
Alex Matson <alex.matson@digitalasset.com>
2021-09-16 11:22:42 -0400

    test 11

--- publish.sh
+++ publish.sh
@@ -3,15 +3,20 @@
 set -euo pipefail
 
 GIT_TAG=$(git tag --contains)
-GIT_TAG=v1.0.0-rc.1
 RELEASE_TAG="latest"
 
-echo "Found git tag $GIT_TAG..."
+if [[ $GIT_TAG -n ]]
+then
+    echo "No git tag found! Aborting..."
+    exit 1
+fi
+
+echo "Found git tag: $GIT_TAG..."
 
 if [[ $GIT_TAG =~ ^v.*rc.* ]]
 then
     RELEASE_TAG="prerelease"
 fi
 
-echo "Publishing as $RELEASE_TAG..."
+echo "Publishing with `--tag $RELEASE_TAG...`"
 npm publish --access public --tag $RELEASE_TAG --dry-run
